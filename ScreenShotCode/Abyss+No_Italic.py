262ef44a1cfe7525000b1c72b9d09e397828c1b6
Veerhan-glitch <83816165+Veerhan-glitch@users.noreply.github.com>
2021-06-15 23:10:05 +0530

    Initial Upload
    u

---
+++ ScreenShotCode/Abyss+No_Italic.py
@@ -0,0 +1,15 @@
+import math
+
+
+def main(a, b):
+    return math.lcm(a, b)
+
+
+a = 7
+b = 4
+c = True
+print("The lcm of 7 and 4 is", main(1, 2))
+# This is a singal line comment
+'''This is 
+a multi line comment'''
+# Highliting
